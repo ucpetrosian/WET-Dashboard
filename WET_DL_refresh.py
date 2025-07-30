63a4c72721a3e91d8b81e76fcfd268bc62f79847
Audrey P. <156370139+ucpetrosian@users.noreply.github.com>
2025-07-30 11:54:11 -0700

    working_data_refresh code, add data

--- WET_DL_refresh.py
+++ WET_DL_refresh.py
@@ -11,8 +11,10 @@ import folium as fl
 from streamlit_folium import st_folium
 import streamlit as st
 import numpy as np
+from pathlib import Path
 import os
-import glob
+import sys
+import shutil
 import geopandas as gpd
 from IPython.display import display
 import plotly
@@ -21,10 +23,73 @@ from shapely.geometry import Point
 import datetime
 
 
+avg_dict = dict()
+
+cols = ["BattV_Avg", "RH_Avg", "TA_Avg", "e_sat_probe_Avg", "e_probe_Avg", "VPD_Avg", "T_SOIL_Avg", "T_CANOPY_Avg", "WP_Avg",
+        "SPEC_RED_REFL_Avg", "SPEC_NIR_REFL_Avg", "NDVI_Avg", "ARVI2_Avg", "IPVI_Avg", "DVI_Avg", "SR_Avg", "MSR_Avg",
+        "SWC_1_1_1_Avg", "SWC_1_2_1_Avg", "SWC_1_3_1_Avg", "SWC_1_4_1_Avg", "SWC_1_5_1_Avg", "SWC_1_6_1_Avg",
+        "SWC_1_7_1_Avg", "SWC_1_8_1_Avg", "Ka_1_1_1_Avg", "Ka_1_2_1_Avg", "Ka_1_3_1_Avg", "Ka_1_4_1_Avg",
+        "Ka_1_5_1_Avg", "Ka_1_6_1_Avg", "Ka_1_7_1_Avg", "Ka_1_8_1_Avg", "TS_1_1_1_Avg", "TS_1_2_1_Avg",
+        "TS_1_3_1_Avg", "TS_1_4_1_Avg", "TS_1_5_1_Avg", "TS_1_6_1_Avg", "TS_1_7_1_Avg", "TS_1_8_1_Avg",
+        "SEC_1_1_1_Avg", "SEC_1_2_1_Avg", "SEC_1_3_1_Avg", "SEC_1_4_1_Avg", "SEC_1_5_1_Avg", "SEC_1_6_1_Avg",
+        "SEC_1_7_1_Avg", "SEC_1_8_1_Avg", "SW_IN_Avg", "SW_OUT_Avg", "LW_IN_Avg", "LW_OUT_Avg", "NETRAD_Avg",
+        "PPFD_BC_IN_Avg", "PPFD_IN_Avg"]
+
+for param_name in cols:
+  avg_dict[param_name] = param_name.replace("_Avg", "")
+
+gen_df = pd.DataFrame()
+ind_df = pd.DataFrame()
+soil_df = pd.DataFrame()
+rad_df = pd.DataFrame()
+
 sites = ["WET01", "WET02", "WET03"]
-columns = []
-path = "C:/Users/cpetrosi/Box/TREX/MISCELLANEOUS/Datalogger_Report_Files/"
+path = Path("C:/Users/cpetrosi/Box/TREX/MISCELLANEOUS/Datalogger_Report_Files/")
 for i in range(0, len(sites)):
-  for name in glob.glob(path + "*"+sites[2]+"*"):
-    print(name)
-    columns.append(pd.read_csv(name, header = [0], skiprows = [0,2,3]).columns)
+  for name in path.glob("*"+sites[i]+"*.dat"):
+    if str(name).split(".")[0].split("_")[-1] == "General":
+      temp = pd.read_csv(name, header = [0], skiprows = [0,2,3])
+      if sites[i] == "WET01":
+        temp = temp[["TIMESTAMP", "BattV_Avg", "RH_Avg", "TA_Avg", "e_sat_probe_Avg", "e_probe_Avg",
+                        "VPD_Avg", "T_SOIL_Avg", "T_CANOPY_Avg", "WP_Avg", "PPFD_BC_IN_Avg", "PPFD_IN_Avg"]]
+      else:
+        temp = temp[["TIMESTAMP", "BattV_Avg", "RH_Avg", "TA_Avg", "e_sat_probe_Avg", "e_probe_Avg",
+                        "VPD_Avg", "T_SOIL_Avg", "T_CANOPY_Avg", "WP_Avg"]]
+      temp["site"] = sites[i]
+      gen_df = pd.concat([gen_df, temp])
+      print(gen_df)
+        
+    elif str(name).split(".")[0].split("_")[-1] == "Indices":
+      temp = pd.read_csv(name, header = [0], skiprows = [0,2,3])
+      temp = temp[["TIMESTAMP", "SPEC_RED_REFL_Avg", "SPEC_NIR_REFL_Avg", "NDVI_Avg", "ARVI2_Avg",
+                "IPVI_Avg", "DVI_Avg", "SR_Avg", "MSR_Avg"]]
+      temp["site"] = sites[i]
+      ind_df = pd.concat([ind_df, temp])
+
+    elif str(name).split(".")[0].split("_")[-1] == "SOILVUE":
+      temp = pd.read_csv(name, header = [0], skiprows = [0,2,3])
+      temp = temp[["TIMESTAMP", "SWC_1_1_1_Avg", "SWC_1_2_1_Avg", "SWC_1_3_1_Avg", "SWC_1_4_1_Avg",
+                "SWC_1_5_1_Avg", "SWC_1_6_1_Avg", "SWC_1_7_1_Avg", "SWC_1_8_1_Avg",
+                "Ka_1_1_1_Avg", "Ka_1_2_1_Avg", "Ka_1_3_1_Avg", "Ka_1_4_1_Avg",
+                "Ka_1_5_1_Avg", "Ka_1_6_1_Avg", "Ka_1_7_1_Avg", "Ka_1_8_1_Avg",
+                "TS_1_1_1_Avg", "TS_1_2_1_Avg", "TS_1_3_1_Avg", "TS_1_4_1_Avg",
+                "TS_1_5_1_Avg", "TS_1_6_1_Avg", "TS_1_7_1_Avg", "TS_1_8_1_Avg",
+                "SEC_1_1_1_Avg", "SEC_1_2_1_Avg", "SEC_1_3_1_Avg", "SEC_1_4_1_Avg",
+                "SEC_1_5_1_Avg", "SEC_1_6_1_Avg", "SEC_1_7_1_Avg", "SEC_1_8_1_Avg"]]
+      temp["site"] = sites[i]
+      soil_df = pd.concat([soil_df, temp])
+
+    elif str(name).split(".")[0].split("_")[-1] == "Rad":
+      temp = pd.read_csv(name, header = [0], skiprows = [0,2,3])
+      temp = temp[["TIMESTAMP", "SW_IN_Avg", "SW_OUT_Avg",
+                "LW_IN_Avg", "LW_OUT_Avg", "NETRAD_Avg"]]
+      temp["site"] = sites[i]
+      rad_df = pd.concat([rad_df, temp])
+    
+
+p1 = gen_df.merge(ind_df, on = ["TIMESTAMP", "site"], how = "outer")
+p2 = rad_df.merge(soil_df, on = ["TIMESTAMP", "site"], how = "outer")
+
+fin = p1.merge(p2, on = ["TIMESTAMP", "site"], how = "outer")
+
+fin.to_csv("C:/Users/cpetrosi/Documents/GitHub/WET-Dashboard/Static_Files/WET_dashboard_data.csv")
